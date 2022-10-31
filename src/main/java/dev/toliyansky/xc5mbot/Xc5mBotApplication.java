dfc6cf010d215364c97f42fa32389c086545f39a
Anatoliy Kozlov <toliyansky@gmail.com>
2022-10-31 13:51:15 +0400

    Init Spring application.

---
+++ src/main/java/dev/toliyansky/xc5mbot/Xc5mBotApplication.java
@@ -0,0 +1,13 @@
+package dev.toliyansky.xc5mbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xc5mBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xc5mBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/toliyansky/xc5mbot/Xc5mBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.toliyansky.xc5mbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xc5mBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
